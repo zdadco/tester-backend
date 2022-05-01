4e0c8d04ac0280af7f3684d4071353c8fb4844f9
Yaroslav Ryabov <yar.ryabov@mail.ru>
2022-05-01 16:07:01 +0400

    init project

---
+++ src/main/java/ru/zdadco/tester/TesterBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.zdadco.tester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesterBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesterBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/zdadco/tester/TesterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.zdadco.tester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesterBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
