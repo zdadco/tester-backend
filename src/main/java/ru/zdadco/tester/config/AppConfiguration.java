b5cb6c35d12798218971c32a31f99276338df961
test Kalafati <89127651427@mail.ru>
2022-05-12 12:28:28 +0400

    add first test

--- src/main/java/ru/zdadco/tester/config/AppConfiguration.java
+++ src/main/java/ru/zdadco/tester/config/AppConfiguration.java
@@ -1,23 +1,29 @@
 package ru.zdadco.tester.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import ru.zdadco.tester.model.Answer;
 import ru.zdadco.tester.model.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.List;
 import java.util.Map;
 
 @Configuration
+@RequiredArgsConstructor
 public class AppConfiguration {
 
+    private final ResourceLoader resourceLoader;
+
     @Bean
-    public Map<Integer, Test> tests() {
-        List<String> questions = List.of(
-                "Какой цвет лучше всего описывает ваше обычное настроение?",
-                "С каким цветом вы ассоциируете себя?",
-                "Какой ваш любимый цвет?"
-        );
+    public Map<Integer, Test> tests() throws IOException {
+        Resource resource = resourceLoader.getResource("classpath:tests/1");
+        List<String> questions = Files.readAllLines(resource.getFile().toPath());
+
         List<Answer> answers = List.of(
                 new Answer(1, 255,0,0),
                 new Answer(2, 255,165,0),
